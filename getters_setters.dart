5df56bf1d102fbf2a22e3ae4a7c8ab49d3013c68
vidhyaramesh117 <122439441+vidhyaramesh117@users.noreply.github.com>
2023-07-11 15:28:04 +0500

    Add files via upload

---
+++ getters_setters.dart
@@ -0,0 +1,36 @@
+class Student
+{
+  var name;
+  var age;
+  String get stud_name
+  {
+    return name;
+  }
+  void set stud_name(String name)
+  {
+    this.name = name;
+  }
+  void set stud_age(int age)
+  {
+    if(age <= 0)
+    {
+      print("Age should be greater than 5");
+    }
+    else
+    {
+      this.age = age;
+    }
+  }
+  int get stud_age
+  {
+    return age;
+  }
+}
+void main()
+{
+  Student s1 = new Student();
+  s1.stud_name = "MARK";
+  s1.stud_age = 0;
+  print(s1.stud_name);
+  print(s1.stud_age);
+}
