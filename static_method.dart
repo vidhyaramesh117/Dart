056dd91ff8e4ec424c341bd1a2eef7d79568f8cc
vidhyaramesh117 <122439441+vidhyaramesh117@users.noreply.github.com>
2023-07-17 15:36:38 +0500

    Add files via upload

--- static_method.dart
+++ static_method.dart
@@ -1,15 +1,30 @@
 class student
 {
-  var name;
+  static var name;
 
-  static display()
+  static display(String name)
   {
-    print("Student name is");
+    print("Student name is "+name);
   }
 }
 void main()
 {
+  student.name="vidhya";
   student std = student();
-  std.name = "psycho";
-  student.display();
+  
+  student.display("Anu");
 }
+
+// class SetTopBox{
+//   static var Channel;
+//   static changeChannel(String channelName){
+//     print("Change Channel : " + channelName);
+//   }
+// }
+// void main(){
+// // calling static variable Channel using className following . followed by variableName.
+//   SetTopBox.Channel="Sony TV";  
+// // calling static method changeChannel using className following . followed by methodName.
+//  print(SetTopBox.Channel);
+//   SetTopBox.changeChannel("Star TV");
+// }
