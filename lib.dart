f3d54b3bbaeb26d967ca48f4b59e328691fe377a
vidhyaramesh117 <122439441+vidhyaramesh117@users.noreply.github.com>
2023-07-18 15:26:05 +0500

    Add files via upload

---
+++ lib.dart
@@ -0,0 +1,7 @@
+library lib;
+
+int subtract(int num1,int num2)
+{
+  print("Subtract");
+  return num1 - num2;
+}
--- library2.dart
+++ library2.dart
@@ -1,4 +1,5 @@
 import 'library.dart';
+import 'lib.dart' as lib;
 
 void main()
 {
@@ -7,9 +8,11 @@ void main()
 
   var sum = add(num1, num2);
   var mod = modulus(num1, num2);
+  var sub = lib.subtract(num1, num2);
   var r = random(100);
 
   print("$num1 + $num2 = $sum");
   print("$num1 % $num2 = $mod");
+  print("$num1 - $num2 = $sub");
   print("Random num = $r ");
 }
